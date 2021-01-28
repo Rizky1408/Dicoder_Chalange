f00412cfabcf66edab6706ee8691169268b211c4
Rizky1408 <agungdwisatria23@gmail.com>
2021-01-28 17:45:16 +0800

    Upload di Github

---
+++ solved.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    for(int j = 1; j <= 6; j++){
+      for(int x = 6; x >= 1; x--){
+        cout << x <<" ";
+      }
+      break;
+    }
+}
